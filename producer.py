24eb21eb188af126079076f5a3e5280c2915abb8
mmensi64@gmail.com <mmensi64@gmail.com>
2022-12-12 22:36:26 +0100

    initial commit

---
+++ producer.py
@@ -0,0 +1,24 @@
+import json
+import time
+import csv
+from kafka import KafkaProducer
+
+
+def producer():
+    producer = KafkaProducer (
+    bootstrap_servers=['192.168.56.1:9092'],
+    value_serializer=lambda v: json.dumps(v).encode('utf-8'))
+
+    with open('C:/Users/chaou/Desktop/projet3/data.csv') as file:
+
+        reader = csv.DictReader(file, delimiter=";")
+        for row in reader:
+            producer.send(topic='my_topic', value=row)
+            time.sleep(1)
+            print(row)
+            producer.flush()
+
+if __name__ == '__main__':
+
+    #while 1 == 1:
+    producer()
